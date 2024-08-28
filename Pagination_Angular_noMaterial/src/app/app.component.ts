86c2b2b72ac01dfdf7e8d6def012ccd15883c2ee
Dilussi Fernando <122093849+Ash-Dilussi@users.noreply.github.com>
2024-08-28 13:35:46 +0530

    update component

--- Pagination_Angular_noMaterial/src/app/app.component.ts
+++ Pagination_Angular_noMaterial/src/app/app.component.ts
@@ -11,55 +11,56 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   @Input() currentPage = this.pageservice.page;
-  @Input() itemsPerPage= this.pageservice.pageSize;
-  @Input() totalItems: number=100;
+  @Input() itemsPerPage = this.pageservice.pageSize;
+  @Input() totalItems: number = 100;
   @Output() pageChanged: EventEmitter<number> = new EventEmitter();
-  totalPages:any;
-  
+  totalPages: any;
+
   title = 'Pagination_Angular_noMaterial';
-  allItemList: any[]=[];
-  slicedList: any[]=[];
+  allItemList: any[] = [];
+  slicedList: any[] = [];
 
 
-  constructor( public pageservice: PaginationService,
+  constructor(public pageservice: PaginationService,
     private _appService: AppService
-  ){}
+  ) { }
 
 
   ngOnInit(): void {
     this.fetchData();
-   
+
 
   }
 
 
-  changePage(pno: number){
+  changePage(pno: number) {
     console.log("nexxt pressed");
     this.pageservice.page = pno;
-   this.slicedList= this.pageservice.changePage(this.allItemList);
-   this.currentPage = pno;
+    this.slicedList = this.pageservice.changePage(this.allItemList);
+    this.currentPage = pno;
 
-  
 
   }
 
 
-
-  fetchData(){
-    this._appService.getAllItemList().subscribe((data:any)=>{
+  fetchData() {
+    this._appService.getAllItemList().subscribe((data: any) => {
       this.allItemList = [...data];
-      
-     
-     this.totalItems= this.allItemList.length;
-     this.totalPages = Math.ceil(this.totalItems/this.itemsPerPage);
-     const columnNames = this.getColumnNames(this.allItemList);
-     this.allItemList= this.pageservice.onSort(columnNames[2], this.allItemList);
 
-     this.changePage(1);
-     console.log("All Itme count: "+ this.totalItems)
+
+      this.totalItems = this.allItemList.length;
+      this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
+      //const columnNames = this.getColumnNames(this.allItemList);
+      //this.allItemList = this.pageservice.onSort(columnNames[2], this.allItemList);
+      
+      const sortColumnName = Object.keys(this.allItemList[0])[2];
+      this.allItemList = this.pageservice.onSort(sortColumnName, this.allItemList);
+
+      this.changePage(1);
+      console.log("All Itme count: " + this.totalItems)
     })
   }
 
